08d1ba44961aea3323d270703da1223cb2726ee1
Mark Apinis <mapinis2019@gmail.com>
2018-05-22 16:27:41 -0400

    A bit better testing efficiency

--- testing/tests.js
+++ testing/tests.js
@@ -2,16 +2,15 @@ const Bop = require('bop');
 const fs = require('fs');
 
 const pattern = "him";
-const bop = Bop(pattern);
 
 fs.readFile("M4M1.1.txt", (err, data) => {
     if(err) {
         console.log(err);
     } else {
-        const indeces = bop.parse(data);
+        const indeces = Bop(pattern).parse(data);
+        const lineBreaks = Bop("\n").parse(data);
         indeces.forEach(matchIndex => {
-            const lineBreaks = Bop("\n").parse(data);
-            const preBreakIndex = lineBreaks.findIndex((element => element >= matchIndex));
+            const preBreakIndex = lineBreaks.findIndex(element => element >= matchIndex);
             console.log(data.toString().substring(lineBreaks[preBreakIndex - 1], lineBreaks[preBreakIndex]));
         });
     }
