f09ed4d02f53f07a9f7e235db2ba4478c03d2596
Mark Apinis <mapinis2019@gmail.com>
2018-05-23 08:26:10 -0400

    Better Search

--- testing/tests.js
+++ testing/tests.js
@@ -1,18 +1,12 @@
-const Bop = require('bop');
 const fs = require('fs');
+const readline = require('readline');
 
 const pattern = "him";
 
-fs.readFile("M4M1.1.txt", (err, data) => {
-    if(err) {
-        console.log(err);
-    } else {
-        const indeces = Bop(pattern).parse(data);
-        const lineBreaks = Bop("\n").parse(data);
-        indeces.forEach(matchIndex => {
-            const preBreakIndex = lineBreaks.findIndex(element => element >= matchIndex);
-            console.log(data.toString().substring(lineBreaks[preBreakIndex - 1], lineBreaks[preBreakIndex]));
-        });
+const lineReader = readline.createInterface(fs.createReadStream("M4M1.1.txt"));
+lineReader.on("line", line => {
+    if(line.includes(pattern)){
+        console.log(line);
     }
 });
 
