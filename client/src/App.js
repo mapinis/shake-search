e7a80d226193a445925dd344f4934bd240c779e0
Mark Apinis <mapinis2019@gmail.com>
2018-05-25 12:59:05 -0400

    Frontend start

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Menu, Dropdown, Icon } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      chosenPlay: "Hamlet",
+      results: []
+    }
+  }
+
+  changePlay = elem => {
+    this.setState({
+      chosenPlay: elem.key
+    });
+  }
+
+  playMenu = (
+    <Menu onClick={this.changePlay}>
+      <Menu.Item key="Hamlet">
+        Hamlet
+      </Menu.Item>
+      <Menu.Item key="Measure for Measure">
+        Measure for Measure
+      </Menu.Item>
+      <Menu.Item key="Henry V">
+        Henry V
+      </Menu.Item>
+    </Menu>
+  )
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Dropdown overlay={this.playMenu} trigger={['click']}>
+          <div>{this.state.chosenPlay} <Icon type='down' /></div>
+        </Dropdown>
+        <div className="results">
+          {this.state.results.length !== 0 && this.state.results}
+          {this.state.results.length === 0 && (
+            <div>Hit go to search</div>
+          )}
+        </div>
       </div>
     );
   }
