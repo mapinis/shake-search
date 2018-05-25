9aa0444248fcf571a51047a89cb89e7c7ddf42eb
Mark Apinis <mapinis2019@gmail.com>
2018-05-25 15:09:13 -0400

    Some UI changes, going to use search box

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Dropdown, Icon } from 'antd';
+import { Select } from 'antd';
 import 'antd/dist/antd.css';
 import './App.css';
 
@@ -8,37 +8,32 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      chosenPlay: "Hamlet",
+      chosenPlay: "hamlet",
       results: []
     }
   }
 
-  changePlay = elem => {
+  changePlay = value => {
     this.setState({
-      chosenPlay: elem.key
+      chosenPlay: value
     });
   }
 
-  playMenu = (
-    <Menu onClick={this.changePlay}>
-      <Menu.Item key="Hamlet">
-        Hamlet
-      </Menu.Item>
-      <Menu.Item key="Measure for Measure">
-        Measure for Measure
-      </Menu.Item>
-      <Menu.Item key="Henry V">
-        Henry V
-      </Menu.Item>
-    </Menu>
-  )
-
   render() {
+
+    searchAfter = (
+      <Select defaultValue="hamlet" className="playSelect" onChange={this.changePlay}>
+        <Select.Option value="hamlet">Hamlet</Select.Option>
+        <Select.Option value="measure">Measure for Measure</Select.Option>
+        <Select.Option value="henryv">Henry V</Select.Option>
+      </Select>
+    );
+
     return (
       <div className="App">
-        <Dropdown overlay={this.playMenu} trigger={['click']}>
-          <div>{this.state.chosenPlay} <Icon type='down' /></div>
-        </Dropdown>
+        <div className="searchSentence">
+
+        </div>
         <div className="results">
           {this.state.results.length !== 0 && this.state.results}
           {this.state.results.length === 0 && (
